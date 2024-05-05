c3f08477657a8de21f0f93fc513daa62c2ae1583
ASMA-GIT <asmashaheen.shah@gmail.com>
2024-05-06 01:59:07 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./App.css";
 
 import Login from "./pages/login/Login";
